5d909b1ea254a74709493bcbda807954c5a9c2b9
rebeccapeltz <rebeccapeltz@gmail.com>
2023-01-25 15:59:55 -0800

    add react router and sidebar navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact"
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
+import Layout from "./components/Layout";
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route element={<Layout />} >
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Layout.js
@@ -0,0 +1,100 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+
+export default function Layout() {
+
+    const testMenuItems = [
+        {
+            href: '/',
+            title: 'Introduction',
+        },
+        {
+            href: 'about',
+            title: 'About',
+        },
+        {
+            href: 'contact',
+            title: 'Contact',
+        }
+    ];
+    // const menuItems = [
+    //   {
+    //     href: '/',
+    //     title: 'Introduction',
+    //   },
+    //   {
+    //     href: '/animated',
+    //     title: 'Animated',
+    //   },
+    //   {
+    //     href: '/about',
+    //     title: 'Border',
+    //   },
+    //   {
+    //     href: '/contact',
+    //     title: 'Delivery',
+    //   },
+    //   {
+    //     href: '/effect',
+    //     title: 'Effect',
+    //   },
+    //   {
+    //     href: '/namedtransformation',
+    //     title: 'Named Transformation',
+    //   },
+    //   {
+    //     href: '/overlay',
+    //     title: 'Overlay',
+    //   },
+    //   {
+    //     href: '/psdtools',
+    //     title: 'PSDTools',
+    //   },
+    //   {
+    //     href: '/resize',
+    //     title: 'Resize',
+    //   },
+    //   {
+    //     href: '/roundcorners',
+    //     title: 'RoundCorners',
+    //   },
+    //   {
+    //     href: '/reshape',
+    //     title: 'Reshape',
+    //   },
+    //   {
+    //     href: '/videoedit',
+    //     title: 'VideoEdit',
+    //   },
+    // ];
+
+    <li><NavLink to="/">Home</NavLink></li>
+
+
+    return (
+        <div className='min-h-screen flex flex-col'>
+            <header className='bg-gray-200 text-black sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
+                Cloudinary Actions
+            </header>
+            <div className='flex flex-col md:flex-row flex-1'>
+                <aside className='bg-gray-100 w-full md:w-60'>
+                    <nav>
+                        <ul>
+                            {testMenuItems.map(({ href, title }) => (
+                                <li className='m-2' key={title}>
+                                    <NavLink to={href} >
+                                        <p className={'text-black'}>{title}</p>
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </aside>
+                <main className={'flex-1'}>
+                    <Outlet />
+                </main>
+                {/* <main className='flex-1'>{children}</main> */}
+            </div>
+        </div>
+    );
+}
