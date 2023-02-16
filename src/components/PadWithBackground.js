19e03bbded179d9f6897b75ba0597bf5d0f4ed7d
rebeccapeltz <rebeccapeltz@gmail.com>
2023-02-15 16:32:38 -0800

    pad with background auto image

--- src/components/PadWithBackground.js
+++ src/components/PadWithBackground.js
@@ -1,33 +1,25 @@
-import '../App.css';
+import "../App.css";
 import { Sandpack } from "@codesandbox/sandpack-react";
-
-
 
 const cloudinaryAdvancedImage = `import {AdvancedImage} from '@cloudinary/react';
 import {Cloudinary} from "@cloudinary/url-gen";
 import {pad} from '@cloudinary/url-gen/actions/resize';
-//import {backgroundColor} from '@cloudinary/url-gen/actions/backgroundColor';
-
+import { color } from "@cloudinary/url-gen/qualifiers/background";
 export default function App() {
    
   const cld = new Cloudinary({ cloud: {cloudName: 'demo'}});
-  const myImage = cld.image('cld-sample').resize(pad().width(350).height(100));
-
-
-  //const image1 = cld.image().resize(pad().width(300).height(100)); 
-  //const image2 = cld.image().resize(pad().width(300).height(100)).backgroundColor("auto");
+  const myImage = cld.image('cld-sample').resize(pad().width(350).height(100).background(color("#FF0000")));
   return (
     <div>
       <AdvancedImage cldImg={myImage} />
     </div>
   )
 }
-`
+`;
 // const cloudinaryAdvancedVideo = `
 // import {AdvancedVideo} from '@cloudinary/react';
 // import {CloudinaryVideo} from "@cloudinary/url-gen";
 // import {scale} from '@cloudinary/url-gen/actions/resize';
-
 
 // const video = new CloudinaryVideo('climbing',{cloudName:'cloudinary-training',analytics:false});
 // video.resize(scale().width(350));
@@ -41,36 +33,41 @@ export default function App() {
 // `
 
 export default function PadWithBackground() {
-
-    return (
-        <div className="code-container">
-            <h3 className={'font-medium leading-tight text-3xl mt-0 mb-2 text-white-600'}>Pad with Backgroud</h3>
-            <Sandpack
-                theme="dark"
-                template="react"
-                files={{
-                    "/App.js": cloudinaryAdvancedImage,
-                }}
-                customSetup={{
-                    dependencies: {
-                        "@cloudinary/react": "^1.9.0",
-                        "@cloudinary/url-gen": "^1.8.7",
-                    },
-                }}
-                options={{
-                    showNavigator: true,
-                    showTabs: true,
-                    showLineNumbers: true, // default - true
-                    showInlineErrors: true, // default - false
-                    wrapContent: true, // default - false
-                    editorHeight: 500, // default - 300
-                    autorun: false,
-                    recompileMode: "delayed", //default is immediate
-                    recompileDelay: 400,
-                    resizablePanels: true, //default
-                }}
-            />
-            {/* <h3 className={'font-medium leading-tight text-3xl mt-0 mb-2 text-white-600'}>Scale Video</h3>
+  return (
+    <div className="code-container">
+      <h3
+        className={
+          "font-medium leading-tight text-3xl mt-0 mb-2 text-white-600"
+        }
+      >
+        Pad with Backgroud
+      </h3>
+      <Sandpack
+        theme="dark"
+        template="react"
+        files={{
+          "/App.js": cloudinaryAdvancedImage,
+        }}
+        customSetup={{
+          dependencies: {
+            "@cloudinary/react": "^1.9.0",
+            "@cloudinary/url-gen": "^1.8.7",
+          },
+        }}
+        options={{
+          showNavigator: true,
+          showTabs: true,
+          showLineNumbers: true, // default - true
+          showInlineErrors: true, // default - false
+          wrapContent: true, // default - false
+          editorHeight: 500, // default - 300
+          autorun: false,
+          recompileMode: "delayed", //default is immediate
+          recompileDelay: 400,
+          resizablePanels: true, //default
+        }}
+      />
+      {/* <h3 className={'font-medium leading-tight text-3xl mt-0 mb-2 text-white-600'}>Scale Video</h3>
             <Sandpack
                 theme="dark"
                 template="react"
@@ -96,7 +93,6 @@ export default function PadWithBackground() {
                     resizablePanels: true, //default
                 }}
             /> */}
-        </div>
-    )
+    </div>
+  );
 }
-
